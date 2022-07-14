dab15ec8b7836d5ccf9ffea6d36984c0f884d5f8
ShinyXek <92219959+ShinyXek@users.noreply.github.com>
2022-07-14 16:18:45 +0200

    Tworzenie Postaci z 5city
    esx_identity Z 5city

---
+++ esx_identity/locales/en.lua
@@ -0,0 +1,11 @@
+Locales['en'] = {
+  ['show_registration'] = 'pokaz menu rejestracji',
+  ['show_active_character'] = 'pokaz aktywna postac',
+  ['delete_character'] = 'usun swoja postac i stworz nowa',
+  ['deleted_character'] = 'twoja postac zostala usunieta.',
+  ['not_registered'] = 'nie masz zarejestrowanej postaci.',
+  ['active_character'] = '~b~aktywna postac:~s~ %s %s',
+  ['already_registered'] = 'masz juz zarejestrowana postac.',
+  ['failed_identity'] = 'nie udalo się ustawic twojej postaci, sprobuj ponownie pozniej lub skontaktuj się z administratorem serwera!',
+  ['create_a_character'] = 'aby grac, musisz stworzyc postac.'
+}
